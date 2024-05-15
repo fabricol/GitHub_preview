2b7548cf9daf526495ed14f1962759e7501086b4
fabricol <fabricioctnmiranda@gmail.com>
2024-05-15 09:32:34 -0300

    atualização atividade

--- src/App.jsx
+++ src/App.jsx
@@ -3,86 +3,78 @@ import React, { useState } from "react";
 function App() {
   const [peso, setPeso] = useState(0);
   const [altura, setAltura] = useState(0);
+  const [imcResult, setImcResult] = useState(null); 
+  const [isIMCCalculated, setIsIMCCalculated] = useState(false); 
 
-  const IMC = () => {
-    const alturaM = parseFloat(altura) / 100;
-    const alturaAltura = parseFloat(alturaM) * parseFloat(alturaM);
-    const resultado = parseFloat(peso) / alturaAltura;
-    const resultadoFormatado = resultado.toFixed(2);
-    if (resultadoFormatado < 17) {
-      return "Muito abaixo do peso " + resultadoFormatado;
-    } else if (resultadoFormatado < 18.49) {
-      return "Abaixo do peso " + resultadoFormatado;
-    } else if (resultadoFormatado < 24.99) {
-      return "Peso normal " + resultadoFormatado;
-    } else if (resultadoFormatado < 29.99) {
-      return "Acima do peso " + resultadoFormatado;
-    } else if (resultadoFormatado < 34.99) {
-      return "Obesidade grau 1 " + resultadoFormatado;
-    } else if (resultadoFormatado < 39.99) {
-      return "Obesidade grau 2 (Severa) " + resultadoFormatado;
-    } else {
-      return "Obesidade grau 3 (Mórbida) " + resultadoFormatado;
-    }
-    
+  const classifyIMC = (imc) => {
+    const imcRanges = [
+      { min: 0, max: 17, classification: "Muito abaixo do peso" },
+      { min: 17, max: 18.5, classification: "Abaixo do peso" },
+      { min: 18.5, max: 25, classification: "Peso normal" },
+      { min: 25, max: 30, classification: "Acima do peso" },
+      { min: 30, max: 35, classification: "Obesidade grau 1" },
+      { min: 35, max: 40, classification: "Obesidade grau 2 (Severa)" },
+      { min: 40, max: Infinity, classification: "Obesidade grau 3 (MÃ³rbida)" }
+    ];
+
+    const { classification } = imcRanges.find(
+      range => imc >= range.min && imc < range.max
+    );
+
+    return classification;
   };
+  
+  const calculateIMC = () => {
+    const alturaM = parseFloat(altura) / 100; 
+    const alturaAltura = alturaM * alturaM; 
+    const resultado = parseFloat(peso) / alturaAltura;  
+    const resultadoFormatado = resultado.toFixed(2); 
+
+  const classification = classifyIMC(resultadoFormatado);
+
+  setImcResult(`${classification} ${resultadoFormatado}`);
+  setIsIMCCalculated(true);
+  };
+
+
+  const handleInputChange = (e) => {
+    setIsIMCCalculated(false); 
+    if (e.target.id === "peso") {
+      setPeso(e.target.value);  
+    } else if (e.target.id === "altura") {
+      setAltura(e.target.value); 
+    }
+  };
+
+    
 
   
 
   return (
-    <div>
+    <div className="container">
+      <header>
+
+        
+      </header>
       <form className="formulario">
         <div className="peso">
           <h2>Peso (Kg)</h2>
-          <input type="number" onChange={(evento) => setPeso(evento.target.value)} />
+          <input type="number" id="peso" onChange={handleInputChange} />
         </div>
         <div className="altura">
           <h2>Altura (Cm)</h2>
-          <input type="number" onChange={(evento) => setAltura(evento.target.value)} />
+          <input type="number" id="altura" onChange={handleInputChange} />
         </div>
       </form>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>IMC</th>
-            <th>Classificação</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Menor que 17</td>
-            <td>Muito abaixo do peso</td>
-          </tr>
-          <tr>
-            <td>Entre 17 e 18,49</td>
-            <td>Abaixo do peso</td>
-          </tr>
-          <tr>
-            <td>Entre 18,5 e 24,99</td>
-            <td>Peso normal</td>
-          </tr>
-          <tr>
-            <td>Entre 25 e 29,99</td>
-            <td>Acima do Peso</td>
-          </tr>
-          <tr>
-            <td>Entre 30 e 34,99</td>
-            <td>Obesidade I</td>
-          </tr>
-          <tr>
-            <td>Entre 35 e 39,99</td>
-            <td>Obesidade II (severa)</td>
-          </tr>
-          <tr>
-            <td>Acima de 40</td>
-            <td>Obesidade III (Mórbida)</td>
-          </tr>
-        </tbody>
-      </table>
-    <div className="peso" >
-      <h2>Resultado :</h2>
-      {IMC()}
-    </div>  
+      {isIMCCalculated && ( 
+        <div className="resultado">
+          <h2>Resultado :</h2>
+          {imcResult}
+        </div>
+      )}
+      <div className="peso">
+        <button onClick={calculateIMC}>Calcular IMC</button> 
+      </div>
     </div>
   );
 }
